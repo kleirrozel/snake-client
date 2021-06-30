92bd6c8feda423bad84228ac4f596712ff181773
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-30 15:39:59 -0400

    Added the stdin setup to play file

--- play.js
+++ play.js
@@ -1,5 +1,31 @@
-const net = require("net");
-const connect = require("./client")
+const { stdin } = require("process");
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  if (key === '\u0003') { /// ctrl+c which exits
+    process.exit();
+  } else if (key === 'w') {
+    
+  } else if (key === 'a') {
+
+  } else if (key === 's') {
+
+  } else if (key === 'd') {
+
+  return handleUserInput;
+};
+
+setupInput();
